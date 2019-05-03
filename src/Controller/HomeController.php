38fa5a2fd9d0f3f3369aea782887fba16e211051
Evgeniy Achin <sozdatel@leoaprt.kz>
2019-05-03 20:13:32 +0600

    update controller 2

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -19,7 +19,7 @@ class HomeController extends AbstractController
     {
         //$user=new User('test@app.test','2');
         //
-        return $this->render('app/home.html.twig',['email'=>'test@auto.kz']);
+        return $this->render('app/home.html.twig',['email'=>'my@auto.kz']);
         #return new JsonResponse(['test'=>1]);
     }
 
